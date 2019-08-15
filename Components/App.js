cd9b0c4e63adb9ae3957fe23ce1343aa9928fdde
Sam Joseph <sjoseph92@gmail.com>
2019-08-15 08:15:13 -0700

    [feat] refactored App.js to use React Navigate

--- Components/App.js
+++ Components/App.js
@@ -13,76 +13,20 @@ import {
 import UserHomePage from './UserHomePage'
 import SignUpForm from './SignUpForm'
 import SignInForm from './SignInForm'
-import { SwitchNavigator } from 'react-navigation'
-
-class App extends Component {
-
-  state = {
-    username: '',
-    password: '',
-    isLoggedIn: false,
-    newUser: false
-  }
-
- 
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
 
 
 
-  render() {
-    if(this.state.isLoggedIn || (this.state.isLoggedIn && this.state.newUser)) {
-      return <UserHomePage />
-    }
-    if(this.state.newUser && !this.state.isLoggedIn) {
-      return <SignUpForm />
-    }
-   
-    return (
-      <SignInForm />
-    );
-  }
+const App = createSwitchNavigator({
+  SignInForm,
+  SignUpForm,
+  UserHomePage
+},
+{
+  initialRouteName: 'SignInForm'
 }
+)
 
-const styles = StyleSheet.create({
-  WelcomeText: {
-    color: "white",
-    textAlign: "center",
-    flexDirection: "column",
-    fontSize: 50
-  },
-  MainContainer: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#6D54A9",
-    flexDirection: "column"
-  },
+export default createAppContainer(App)
 
-  SubmitButtonStyle: {
-    height: 50,
-    marginTop: 10,
-    paddingTop: 15,
-    paddingBottom: 15,
-    marginLeft: 30,
-    marginRight: 30,
-    backgroundColor: "transparent",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#fff"
-  },
 
-  TextStyle: {
-    color: "#fff",
-    textAlign: "center"
-  },
-  textInput: {
-    fontSize: 18,
-    backgroundColor: 'rgba(255,255,255,0.1)',
-    //color: 'white',
-    height: 50,
-    justifyContent: "center",
-    marginBottom: 15,
-    marginLeft: 30,
-    marginRight: 30
-  }
-});
-
-export default App;
